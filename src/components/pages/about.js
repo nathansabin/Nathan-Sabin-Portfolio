d1ac0d2394ef08d92a809e1f2c21a32b67c3a8f4
nathansabin <sabinnat001@gmail.com>
2023-08-13 23:05:25 -0700

    added portrait readme and contact fixes

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
+import nathan from '../../assets/images/portrait.png';
 
 export default function About() {
     return (
-        <div class='box-set'>
-            <div class='box-about'>
-                <p class='about'>
-                Welcome to my portfolio! I have been learning computer science and coding for over a year, this skill has become a great obsession for me. 
-                I went through a 24-week full stack coding bootcamp, and before that, I spent time learning the basics of Python, C, and JS.
-                My greatest interest is in back-end web development, but I have practiced front-end as well.
-                </p>
+        <>
+            <div className='box-set'>
+                <div className='image portrait'>
+                        <img src={nathan} alt='nathan'></img>
+                </div>
             </div>
-        </div>
+            <div className='box-set'>
+                <div className='box-about'>
+                    <p className='about'>
+                    Welcome to my portfolio! I have been learning computer science and coding for over a year, this skill has become a great obsession for me. 
+                    I went through a 24-week full stack coding bootcamp, and before that, I spent time learning the basics of Python, C, and JS.
+                    My greatest interest is in back-end web development, but I have practiced front-end as well.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,29 +1,53 @@
 import React, { useState } from 'react';
 
 export default function Contact() {
-    const sendEmail = (event) => {
-        event.preventDefault();
-
-
-    }
+    const [name, setName] = useState(false);
+    const [email, setEmail] = useState(false);
+    const [feild, setFeild] = useState(true);
+    const [buttonText, setButtonText] = useState('submit');
 
     const checkName = () => {
+        const newName = document.getElementById('name').value;
+        const namePattern = /^[a-z]/i;
+        setName(namePattern.test(newName));
 
+        const newFeild = checkFeild();
+        setFeild(newFeild);
+        buttonName();
     }
 
     const checkEmail = () => {
-
+        const newEmail = document.getElementById('email').value;
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        setEmail(emailPattern.test(newEmail));
+        
+        const newFeild = checkFeild();
+        setFeild(newFeild);
+        buttonName();
     }
 
+    const checkFeild = () => {
+        return !(name && email);
+    }
+
+    const buttonName = () => {
+        if (!name) {
+            setButtonText('invalid name');
+        } else if (!email) {
+            setButtonText('invalid email');
+        } else {
+            setButtonText('submit');
+        }
+    }
 
     return (
-        <div>
+        <div className='bottom-page'>
             <div className='email-format bottom-page'>
-                <form>
-                    <input id='name' className='email-info' type='text' placeholder='Name'></input>
-                    <input id='email' className='email-info' type='email' placeholder='Email'></input>
-                    <textarea id='text' className='email-message' rows='4' placeholder='email body'></textarea>
-                    <input id='submit'className='email-button' type='submit' placeholder='submit'></input>
+                <form  action="https://formsubmit.co/98996af15ee44ab3e1a763ac90f5a0be" method="POST">
+                    <input name='name' id='name' className='email-info' type='text' placeholder='Name' onBlur={checkName}></input>
+                    <input name='email' id='email' className='email-info' type='email' placeholder='Email'onBlur={checkEmail}></input>
+                    <textarea name='text' id='text' className='email-message' rows='4' placeholder='email body'></textarea>
+                    <input disabled={feild} id='submit'className='email-button' type='submit' placeholder='submit' value={buttonText}></input>
                 </form>
             </div>                                                  
         </div>
