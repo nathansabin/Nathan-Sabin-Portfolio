73ff719795dee33b3aab571ae5995c66b11ae99b
nathansabin <sabinnat001@gmail.com>
2023-11-08 19:19:48 -0800

    adjusted resume style

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -5,34 +5,43 @@ import pdf from "../../assets/Nathan Sabin.pdf";
 export default function Resume() {
     return (
         <div className='resume-format'>
-            <a href={pdf}><button className="pdf">Resume PDF</button></a>
             <div className='hard-skills'>
-                <nav className='tag-name'>
-                    <h2>Hard Skills</h2>
-                </nav>
-                <div className="list">
-                    <ol className='list-box'>
-                        <h4>MERN Stack</h4>
-                        <li>Mongodb</li>
-                        <li>Express.js</li>
-                        <li>React.js</li>
-                        <li>Node.js</li>
-                    </ol>
-                    <ol className='list-box'>
-                        <h4>Languages</h4>
-                        <li>JavaScript ES6+</li>
-                        <li>Python3</li>
-                        <li>C# 6.0</li>
-                        <li>HTML5</li>
-                    </ol>
-                    <ol className='list-box'>
-                        <h4>Back-End</h4>
-                        <li>MYSQL</li>
-                        <li>SQLite</li>
-                        <li>GraphQL</li>
-                        <li>Flask</li>
-                    </ol>
-                </div>
+            <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th/>
+                    <th scope="col"> <a href={pdf}><button className="pdf btn btn-primary btn-lg">Resume PDF</button></a></th>
+                    <th/>
+                </tr>
+                <tr class="heads">
+                    <th scope="col">MERN Stack</th>
+                    <th scope="col">Languages</th>
+                    <th scope="col">Back-end</th>
+                </tr>
+            </thead>
+            <tbody class="tables">
+                <tr class="table-secondary">
+                    <td>MongoDB</td>
+                    <td>JavaScript ES6+</td>
+                    <td>ASP.NET web API</td>
+                </tr>
+                <tr class="table-secondary">
+                    <td>Express.js</td>
+                    <td>c# 7.0+</td>
+                    <td>mySQL</td>
+                </tr>
+                <tr class="table-secondary">
+                    <td>react.js</td>
+                    <td>Python3</td>
+                    <td>MSSQL</td>
+                </tr>  
+                <tr class="table-secondary">
+                    <td>node.js</td>
+                    <td>HTML5</td>
+                    <td>SQLite</td>
+                </tr>
+            </tbody>
+            </table>
             </div>
         </div>
     )
